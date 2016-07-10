44645157d7fa0ba4f54acf28b6161f7ca59a92c8
frg316 <frg316@lehigh.edu>
2016-07-09 20:26:59 -0400

    gentelella update

--- app/routes.js
+++ app/routes.js
@@ -29,7 +29,7 @@ var Nerd = require('./models/nerd');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('*', function(req, res) {
-            res.sendfile('./public/views/index.html'); // load our public/index.html file
+            res.sendfile('./public/views/gentelella/production/index.html'); // load our public/index.html file
         });
 
     };
